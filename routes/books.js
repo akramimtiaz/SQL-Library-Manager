acb738e55536da7bcb44a0ee1d8e6e79ff15abef
Akram Imtiaz <akram.imtiaz10@gmail.com>
2019-06-23 19:09:03 +1000

    Add Catch to Each Query

--- routes/books.js
+++ routes/books.js
@@ -10,12 +10,14 @@ const determineQueryType = require('./misc/determineQueryType')
 router.get('/', (req,res) => {
     Book.findAll()
     .then((books) => res.render('books/index', { books, title: 'Book Library', search: {} }))
+    .catch(error => res.send(500, error))
 })
 
 router.get('/new', (req, res) => {
     res.render('books/new-book', { book: {}, title: 'New Book' })
 })
 
+//SEARCH
 router.post('/search', (req, res) => {
     const search = req.body
     const where = determineQueryType(search.query, search.field)
@@ -39,6 +41,7 @@ router.get('/:id', (req, res) => {
     const id = req.params.id
     Book.findByPk(id)
     .then(book => res.render('books/update-book', { book, title: 'Update Book' }))
+    .catch(error => res.send(500, error))
 })
 
 //UPDATE
@@ -47,6 +50,7 @@ router.post('/:id', (req, res) => {
     Book.findByPk(id)
     .then(book => book.update(req.body))
     .then(() => res.redirect('/books/'))
+    .catch(error => res.send(500, error))
 })
 
 //DELETE
@@ -55,6 +59,7 @@ router.post('/:id/delete', (req, res) => {
     Book.findByPk(id)
     .then(book => book.destroy())
     .then(() => res.redirect('/books/'))
+    .catch(error => res.send(500, error))
 })
 
 module.exports = router
